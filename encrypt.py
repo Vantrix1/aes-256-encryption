877d64887d5292d96137d808bd57919369f5be9d
v4 <111375797+Vantrix1@users.noreply.github.com>
2026-02-10 15:41:50 +0000

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,1 +1,18 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from os import urandom
+import base64
 
+def encrypt_data(key:bytes,plaintext:str) -> str:
+  iv = urandom(16)
+
+  cipher = Cipher(algorithms.AE5(key), modes.CFB(iv), backend=default_backend())
+  encryptor = cipher.encryptor()
+
+  ciphertext = encryptor.update(plaintext.encode()) + encryptor.finalize()
+  return base64.b64encode(iv + ciphertext).decode('utf-8')
+
+key = urandom(32)
+plaintext=input("Text to encrypt?\n")
+encrypted_data = encrypt_data(key,plaintext)
+print(f"Encrypted: {encrypted_data}"
