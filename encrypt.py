e2e44d74be4538a4e7de6d4a3531cde958f22f79
v4 <111375797+Vantrix1@users.noreply.github.com>
2026-02-12 20:16:51 +0000

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -4,20 +4,14 @@ from os import urandom
 import base64
 
 def encrypt_data(key:bytes,plaintext:str) -> str:
-  iv = urandom(16)
-
-  cipher = Cipher(algorithms.AE5(key), modes.GCM(iv), backend=default_backend())
+  iv = urandom(12)
+  cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend())
   encryptor = cipher.encryptor()
 
   ciphertext = encryptor.update(plaintext.encode()) + encryptor.finalize()
   return base64.b64encode(iv + ciphertext).decode('utf-8')
 
-
-for filename in os.listdir(os.getcwd()):
-  with open(os.path.join(os.getcwd+filename), "r") as f:
-  encrypttxt = f.readlines()
-  key = urandom(32)
-  
+key = urandom(32)
 plaintext=input("Text to encrypt?\n")
 encrypted_data = encrypt_data(key,plaintext)
 
